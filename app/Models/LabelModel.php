ae28403750d1a0cc79a0275a8795b016111497e7
pancasona28 <nuariananda28@gmail.com>
2023-10-15 22:04:12 +0700

    Deploy v-1.0
    sudah selesai

--- app/Models/LabelModel.php
+++ app/Models/LabelModel.php
@@ -4,26 +4,37 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class LabelModel extends Model
-{
+class LabelModel extends Model {
     protected $table      = 'c_w';
     // protected $useTimestamps = true;
     // protected $dateFormat    = 'datetime';
     // protected $createdField  = 'created_at';
     // protected $updatedField  = 'updated_at';
-    protected $allowedFields = ['Id', 'Full_text', 'Dataset', 'Lang', 'Label'];
+    protected $allowedFields = ['Label', 'Chek', 'Status'];
     protected $primaryKey = 'id';
 
-    public function search($keyword)
-    {
+    public function search($keyword) {
         return $this->table($this->table)->like('Dataset', $keyword);
     }
-    public function getdatalabel($id = false)
-    {
+    public function getdatalabel($id = false) {
         if ($id == false) {
             return $this->findAll();
         }
 
         return $this->where(['Id' => $id])->first();
+    }
+    public function getsisa() {
+        $sql = "SELECT COUNT(*) as t_belum FROM $this->table WHERE Chek = 0";
+        $t_belum = $this->db->query($sql)->resultID->fetch_assoc();
+        $sql = "SELECT COUNT(*) as t_sudah FROM $this->table WHERE Chek = 1";
+        $t_sudah = $this->db->query($sql)->resultID->fetch_assoc();
+        
+        $data = ['t_sudah' => $t_sudah,
+                    't_belum' => $t_belum,
+        ];
+            
+        
+        
+        return $data;
     }
 }
--- app/Services/App.php
+++ app/Services/App.php
@@ -9,4 +9,18 @@ class App {
 
         return "Pesan yang berhasil di-log: " . $message;
     }
+
+    public function addmenu($controler) {
+        $output = [];
+        $returnCode = 0;
+    
+        // Gantilah perintah berikut sesuai dengan perintah PHP Spark yang ingin Anda jalankan
+        $command = 'php artisan spark make:controller '. $controler;
+    
+        exec($command, $output, $returnCode);
+    
+        // Lakukan sesuatu dengan output atau return code jika diperlukan
+        // Misalnya, tampilkan output ke pengguna atau tangani kesalahan
+    }
+    
 }
